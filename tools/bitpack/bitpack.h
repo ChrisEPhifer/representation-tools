12313a3c1cd62355a02c59c5e288758dfbd69f7f
Chris Phifer <cphifer97@gmail.com>
2017-01-04 21:09:28 -0500

    Finished up function contracts and comments

--- tools/bitpack/bitpack.h
+++ tools/bitpack/bitpack.h
@@ -8,18 +8,69 @@
  *
  * The API provides methods for packing both signed and unsigned data. Packing
  * and unpacking is safe, in that the behavior for edge cases is well-defined.
- * There are methods for packing in both 32 and 64-bit integers, using the
- * standard library stdint.h, with types uint32_t, uint64_t, int32_t, and
- * int64_t.
  */
 
 #include <stdbool.h>
 #include <stdint.h>
 
-bool bitpack_fitsu32();
-bool bitpack_fitsu64();
-bool bitpack_fitss32();
-bool bitpack_fitss64();
+#define WORD_SIZE 64
+
+/*
+ *    Purpose: Check if a value n fits into the specified number of bits.
+ * Parameters: A 64 bit unsigned value n, and an unsigned width (in bits).
+ *    Returns: A boolean (true if n fits into width, false otherwise)
+ *   Contract: It is a checked runtime error to pass a width greater than the
+ *             maximum word size (64).
+ */
+bool bitpack_fitsu(uint64_t n, unsigned width);
+
+/*
+ *    Purpose: Check if a value n fits into the specified number of bits.
+ * Parameters: A 64 bit signed value n, and an unsigned width (in bits).
+ *    Returns: A boolean (true if n fits into width, false otherwise)
+ *   Contract: It is a checked runtime error to pass a width greater than the
+ *             maximum word size (64).
+ */
+bool bitpack_fitss(int64_t n, unsigned width);
+
+/*
+ *    Purpose: Unpack a value of given width and offset from a bit vector.
+ * Parameters: A bit vector (packed data), the least significant bit position,
+ *             and the number of bits (the width).
+ *    Returns: The data contained at the location specified by the parameters.
+ */
+uint64_t bitpack_getu(uint64_t vec, unsigned lsb, unsigned width);
+
+/*
+ *    Purpose: Unpack a value of given width and offset from a bit vector.
+ * Parameters: A bit vector (packed data), the least significant bit position,
+ *             and the number of bits (the width).
+ *    Returns: The data contained at the location specified by the parameters.
+ */
+int64_t bitpack_gets(uint64_t vec, unsigned lsb, unsigned width);
+
+/*
+ *    Purpose: Pack an unsigned value into the bit vector, and return the new
+ *             bit vector.
+ * Parameters: A bit vector, the least significant bit position, the number of
+ *             bits, and the data itself.
+ *    Returns: The resultant bit vector after packing the data.
+ */
+uint64_t bitpack_setu(uint64_t vec, unsigned lsb, unsigned width, uint64_t val);
+
+/*
+ *    Purpose: Pack an unsigned value into the bit vector, and return the new
+ *             bit vector.
+ * Parameters: A bit vector, the least significant bit position, the number of
+ *             bits, and the data itself.
+ *    Returns: The resultant bit vector after packing the data.
+ */
+uint64_t bitpack_sets(uint64_t vec, unsigned lsb, unsigned width, int64_t val);
+
+
+
+
+
 
 
 
